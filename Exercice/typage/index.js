9c9f235aa2296479e2b1f18751e42c3533dbe9f2
Nassim Sakhri <tails01@hotmail.fr>
2023-09-21 16:39:26 +0200

    test

--- Exercice/typage/index.js
+++ Exercice/typage/index.js
@@ -1,9 +1,19 @@
 "use strict";
+// 1er point
 let userName;
+// 2eme point
 let userAge;
+// 3eme point
 let isLogin = false;
+// 4eme point
 let userNames = [];
-userName = "Jean-Louis";
-userAge = 1272456257;
-userNames.push(userName);
-console.log(userNames);
+// 5eme point
+let Person = {
+    firstName: "Dupont",
+    age: 20,
+    isLogin: true,
+};
+// Est-il possible d'afficher la valeur age via console.log :
+// Reponse : oui
+console.log(Person.age);
+// 6eme point
