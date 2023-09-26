5556c2fa1b9ebab18d7d1ddc613686412a32c874
Nassim Sakhri <tails01@hotmail.fr>
2023-09-26 17:26:10 +0200

    Exercie type Union

---
+++ Exercice/Uniontype/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let person1 = {
+    nom: "Toto Dupont",
+    age: 35,
+    occupation: "Facteur"
+};
+let person2 = {
+    nom: "Jeanne Doe",
+    age: 25,
+    role: "Admin",
+};
+let person3 = {
+    nom: "Michael Flinch",
+    age: 23,
+    occupation: "Lutteur",
+};
+let person4 = {
+    nom: "Michael Flinch",
+    age: 64,
+    role: "Gérant",
+};
+let tableau = [person1, person2, person3, person4];
+for (let i = 0; i < tableau.length; i++) {
+    console.log(tableau[i].nom + " " + tableau[i].age);
+}
---
+++ Exercice/Uniontype/index.ts
@@ -0,0 +1,51 @@
+interface User  {
+    nom: string | number
+    age: number
+    occupation : string
+}
+
+interface Admin {
+    nom: string
+    age: number
+    role : string
+}
+
+type Personne = {
+    nom: string,
+    age: number,
+    occupation? : string,
+    role? : string,
+}
+
+let person1: Personne = {
+    nom: "Toto Dupont",
+    age: 35,
+    occupation:"Facteur"
+}
+
+let person2:  Personne = {
+    nom: "Jeanne Doe",
+    age: 25,
+    role:"Admin",
+}
+
+let person3: Personne = {
+    nom: "Michael Flinch",
+    age: 23,
+    occupation:"Lutteur",
+}
+
+let person4: Personne = {
+    nom: "Michael Flinch",
+    age: 64,
+    role:"Gérant",
+}
+
+let tableau = [person1, person2, person3, person4]
+
+for(let i=0; i<tableau.length;i++){
+    console.log(tableau[i].nom + " " + tableau[i].age)
+}
+
+
+
